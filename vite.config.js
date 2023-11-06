dfab4949830ef7ade6958c029fed7ec59d4a3703
LazyYuuki <huy2840@gmail.com>
2023-11-06 22:45:29 +0800

    🔨 chore: Update base domain name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/landing/",
+  base: "https://caviato.xyz/",
   plugins: [svelte()],
 })
